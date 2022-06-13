d16a43f9e066e1a479c2b4434ba62a0c8cdeda04
Samuel Hermany <70418338+samuelhermany@users.noreply.github.com>
2022-06-12 23:13:24 -0300

    Add project files.

---
+++ Business/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public List<Cliente> Clientes(string cliente)
+        {
+            // new Databse.Cliente().Clientes();s
+            var clientes = new List<Cliente>();
+            clientes.Add(new Cliente() { Nome = "Danilo" });
+            return clientes;
+        }
+    }
+}
